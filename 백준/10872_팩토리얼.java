85355b04315c09173509460e075eba941707d265
Chenjae <45038717+Chenjae@users.noreply.github.com>
2021-08-22 22:11:58 +0900

    Update 10872_팩토리얼.java

--- 백준/10872_팩토리얼.java
+++ 백준/10872_팩토리얼.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Main {
+	//팩토리얼
+	//https://www.acmicpc.net/problem/10872
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int num = scanner.nextInt();

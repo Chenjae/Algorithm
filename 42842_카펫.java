a005fed41a7f0b5eefe2011f300d0d71005e0051
INJAE CHOI <45038717+Chenjae@users.noreply.github.com>
2021-11-11 19:43:32 +0900

    42842번 카펫
    https://programmers.co.kr/learn/courses/30/lessons/42842

---
+++ 42842_카펫.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {};
+        int total = brown + yellow;
+        int x, y;
+        
+        for(int i=2; i<total; i++) {
+            if(total % i == 0) {
+                x = total/i;
+                y = i;
+                
+                if((brown == 2*(x+y-2)) && (yellow == (x-2)*(y-2))) {
+                    answer = new int[] {x, y};
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
